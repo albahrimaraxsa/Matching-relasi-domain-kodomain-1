81e7bc3c56f28c759c896f0a56e1a2cef311f0ba
albahrimaraxsa <albahri_maraxsa@yahoo.co.id>
2017-04-25 10:18:44 +0700

    first commit

---
+++ Game Relasi - Matematika Kelas VIII/Assets/GotoScene2.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GotoScene2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToHomePage(){
+		SceneManager.LoadScene ("_SCENE_0");
+	}
+
+	public void ToGame(){
+		SceneManager.LoadScene ("_SCENE_1");
+	}
+		
+
+	public void ToWin(){
+		SceneManager.LoadScene ("_SCENE_2");
+	}
+}
